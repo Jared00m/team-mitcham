86e29512693621be929f68363feaf65baaa5d73d
Jared Mitcham <jaredmitcham@gmail.com>
2025-01-05 14:49:18 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Team Mitcham`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
