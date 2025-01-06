745a6e44fab57aa43d8a280a7c6c7ef17cb972e0
Jared Mitcham <jaredmitcham@gmail.com>
2025-01-05 21:07:44 -0500

    Added blog page with mdx support

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,21 @@ module.exports = {
   plugins: [
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-mdx',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: `blog`,
         path: `${__dirname}/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-remark-images`,
+      options: {
+        // It's important to specify the maxWidth (in pixels) of
+        // the content container as this plugin uses this as the
+        // base for generating different widths of each image.
+        maxWidth: 800,
       },
     },
   ],
